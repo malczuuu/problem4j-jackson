a51040efe787fc4093eaee8608eea3d7d1831627
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-09-20 15:01:07 +0200

    Migrate Gradle from Groovy to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,100 @@
+import com.diffplug.spotless.LineEnding
+
+plugins {
+    `java-library`
+    `maven-publish`
+    id("com.diffplug.spotless") version "7.2.1"
+}
+
+group = "com.github.malczuuu"
+
+if (version == "unspecified") {
+    version = Versioning.getSnapshotVersion(rootProject.rootDir)
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    withSourcesJar()
+    withJavadocJar()
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://jitpack.io/") }
+}
+
+dependencies {
+    api("com.fasterxml.jackson.core:jackson-databind:2.19.2")
+
+    api("com.github.malczuuu:problem4j-core:3.1.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.4")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.4")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.4")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set("Core library implementing RFC7807")
+                url.set("https://github.com/malczuuu/${project.name}")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+            }
+        }
+    }
+}
+
+spotless {
+    format("misc") {
+        target("*.gradle.kts", ".gitattributes", ".gitignore")
+
+        trimTrailingWhitespace()
+        leadingTabsToSpaces(4)
+        endWithNewline()
+        lineEndings = LineEnding.UNIX
+    }
+
+    java {
+        target("src/**/*.java")
+
+        googleJavaFormat("1.28.0")
+        lineEndings = LineEnding.UNIX
+    }
+}
+
+tasks.register("printVersion") {
+    doLast {
+        println("Project version: $version")
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-parameters")
+}
+
+/**
+ * Disable doclint to avoid errors and warnings on missing JavaDoc comments.
+ */
+tasks.withType<Javadoc>().configureEach {
+    (options as StandardJavadocDocletOptions).apply {
+        addStringOption("Xdoclint:none", "-quiet")
+    }
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
