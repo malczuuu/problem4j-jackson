c99c7f400a8a79398e3066bc0a341e0a4f0ee01f
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-11-18 18:18:54 +0100

    Update nmcp to 1.3.0

--- problem4j-jackson/build.gradle.kts
+++ problem4j-jackson/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     id("java-library")
     id("maven-publish")
     id("signing")
-    id("com.gradleup.nmcp").version("1.2.1")
+    id("com.gradleup.nmcp").version("1.3.0")
 }
 
 java {
