39f155e67dfccd10dd15796bde00c656cc3566c0
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-11-20 20:46:16 +0100

    Set pom display name

--- problem4j-jackson3/build.gradle.kts
+++ problem4j-jackson3/build.gradle.kts
@@ -34,7 +34,7 @@ publishing {
             from(components["java"])
 
             pom {
-                name = project.name
+                name = "Problem4J Jackson3"
                 description = "Jackson3 integration for library implementing RFC7807"
                 url = "https://github.com/malczuuu/${rootProject.name}"
                 inceptionYear = "2025"
